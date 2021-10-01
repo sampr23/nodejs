e0c7fc6faca930d8e3ebd3430d50ef5b206d5696
sampr23 <75003739+sampr23@users.noreply.github.com>
2021-10-01 14:50:10 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
+const bodyparser = require('body-parser');
+const express = require('express');
+var app = express();
 
-var express = require('express');
-var app = express(); 
-var jquery = require('jquery');
 
-app.use(express.static(__dirname + '/public'));
-app.get('/ajaxcall', function(req, res) {
-    var data ={
-        contactId: 1,
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'john.doe@domain.com',
-        phone: '12345'
-    };
-    res.send(data);
+var PORT = 5500;
+
+app.use(bodyparser.urlencoded({extended:true}))
+  
+app.get('',(req,res)=>{
+ 
+  res.sendFile(__dirname+"/submission.html")
+  res.sendFile(__dirname+"/assets/images/form-icon.png")
+  res.sendFile(__dirname+"/assets/js/index.js")
 })
 
-app.listen(5500);
+app.post('/',(req,res)=>{
+  const n1=Number(req.body.num1);
+  const n2=Number(req.body.num2);
+  let floors = n2;
+  let appartement = n1;
+  let appDivFloors = appartement / floors;
+  let elevator = appDivFloors / 6;
+  let elevatortotal = Math.ceil(elevator);
+  let ifmorethan = Math.ceil((floors-1) / 20);
+  let colonne = 1 + ifmorethan;
+  let prix =elevatortotal * 7565;
+  let fees = prix * 0.1;
+  let totalprice = prix + elevatortotal;
+
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('elevator requiered: '+ elevatortotal+ '<br/>');
+  res.write('elevator price: '+ prix + '<br/>' );
+  res.write('fees: '+ fees + '<br/>' );
+  res.write('Total price: '+ totalprice + '<br/>' );
+  res.send();
+})
+  
+app.listen(PORT, function(err){
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
